ab2f4d038027d417a418d5c100f39506a741cee4
Duncan <96219296+DuncanDPS@users.noreply.github.com>
2025-07-14 15:23:14 -0600

    Program.cs configuration

--- TechStoreApiRest/Program.cs
+++ TechStoreApiRest/Program.cs
@@ -1,6 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// habilitar el registro de controladores
+builder.Services.AddControllers();
+
+// habilitar el registro de Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Habilita Swagger solo en desarrollo
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Redirige HTTP a HTTPS
+app.UseHttpsRedirection();
+
+// Habilita la autorización
+app.UseAuthorization();
+
+// Mapea los controladores
+app.MapControllers();
 
 app.Run();
