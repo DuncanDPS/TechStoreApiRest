2c392f8a803bb9fd93844334931f8818df6e786e
Duncan <96219296+DuncanDPS@users.noreply.github.com>
2025-08-07 20:55:50 -0600

    Servicio de usuario con metodos de Login y Registro, funcionan correctamente, y el metodo para generar el token funciona correctamente

--- TechStoreApiRest/Controllers/AuthController.cs
+++ TechStoreApiRest/Controllers/AuthController.cs
@@ -1,59 +1,63 @@
-﻿//using Datos;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Servicios.IServicios;
+﻿using Datos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.IServicios;
+using Servicios.DTOS;
 
 
-//namespace TechStoreApiRest.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class AuthController : ControllerBase
-//    {
+namespace TechStoreApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
 
-//        private readonly IUsuarioService _usuarioService;
-//        private readonly ITokenGeneratorService _tokenGeneratorService;
+        private readonly IUsuarioService _usuarioService;
+        //private readonly ITokenGeneratorService _tokenGeneratorService;
 
-//        public AuthController(IUsuarioService usuarioService, ITokenGeneratorService tokenGeneratorService)
-//        {
-//            _usuarioService = usuarioService;
-//            _tokenGeneratorService = tokenGeneratorService;
-//        }
+        public AuthController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+            //_tokenGeneratorService = tokenGeneratorService;
+        }
 
-//        [HttpPost("registrar-usuario")]
-//        public async Task<IActionResult> RegistrarUsuario(UsuarioRegisterDto usuarioDto)
-//        {
-//            // 1. Validar usuario y contrasenia
-//            if (!ModelState.IsValid)
-//            {
-//                return BadRequest(ModelState);
-//            }
-//            try
-//            {
-//                // mapeamos el usuariodto a entidad
-//                var usuario = UsuarioMapper.CategoriaDtoRespToEntity(usuarioDto);
+        [HttpPost("registrar-usuario")]
+        public async Task<IActionResult> RegistrarUsuario(UsuarioRegisterDto usuarioDto)
+        {
+            // 1. Validar usuario y contrasenia
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                
+                var usuarioCreado = await _usuarioService.RegistrarUsuario(usuarioDto);
+                
+                return StatusCode(StatusCodes.Status201Created, usuarioDto); // devuelve 201 Created con el nuevo usuario
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
 
-//                var usuarioCreado = await _usuarioService.RegistrarUsuario(usuario);
-//                var usuarioResp = UsuarioMapper.ToEntityToResponse(usuario);
-//                return StatusCode(StatusCodes.Status201Created, usuarioResp); // devuelve 201 Created con el nuevo usuario
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(new {error = ex.Message});
-//            }
-//        }
-
-//        [HttpPost("iniciar-sesion")]
-//        public async Task<IActionResult> IniciarSesion(UsuarioLoginDto usuario)
-//        {
-//            if (!ModelState.IsValid)
-//            {
-//                return BadRequest(ModelState);
-//            }
-//            try
-//            {
-
-//            }
-//        }
-//    }
-//}
+        [HttpPost("iniciar-sesion")]
+        public async Task<IActionResult> IniciarSesion(UsuarioLoginDto usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var usuarioLog = await _usuarioService.IniciarSesion(usuario);
+                return StatusCode(StatusCodes.Status201Created, usuarioLog);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new {error = ex.Message});
+            }
+        }
+    }
+}
