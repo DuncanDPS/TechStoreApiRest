7d108bbdda5ad2b857b9085d205e2c7b7011a83d
Duncan <96219296+DuncanDPS@users.noreply.github.com>
2025-08-11 16:47:57 -0600

    Add validation and role handling in DTOs and service
    Updated `UsuarioLoginDto` and `UsuarioRegisterDto` to include data annotations for validation on `Email` and `Contrasenia` properties. Added new properties `Nombre` and `Apellidos` in `UsuarioRegisterDto` and set a default role of "Cliente".
    
    In `UsuarioService`, added a check to set the `Rol` to "Cliente" if it is incorrectly set to "string". This ensures proper role assignment and maintains password hashing and user entity saving functionality.

--- Servicios/DTOS/UsuarioLoginDto.cs
+++ Servicios/DTOS/UsuarioLoginDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Servicios.DTOS
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Servicios.DTOS
 {
     public class UsuarioLoginDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required]
         public string Contrasenia { get; set; } = string.Empty;
     }
 }
