89ce5cc2d785a20ed493cd71e707e1e3154a2df3
Duncan <96219296+DuncanDPS@users.noreply.github.com>
2025-08-15 10:31:13 -0600

    Add XML documentation and new review retrieval method
    Updated `ReviewController` with XML comments for `CrearReview`.
    Introduced `ObtenerReviewPorId` method to fetch reviews by ID,
    including logging for better traceability.

--- TechStoreApiRest/Controllers/ReviewController.cs
+++ TechStoreApiRest/Controllers/ReviewController.cs
@@ -18,7 +18,11 @@ namespace TechStoreApiRest.Controllers
             _reviewService = reviewService;
         }
 
-
+        /// <summary>
+        /// Este endpoint hace la funcion de crear una nueva Review de un Producto
+        /// </summary>
+        /// <param name="review">objecto para crear una review</param>
+        /// <returns>devuelve una review creada</returns>
         [HttpPost("crear-review")]
         public async Task<IActionResult> CrearReview(ReviewDtoAddRequest review)
         {
@@ -40,8 +44,17 @@ namespace TechStoreApiRest.Controllers
             Log.Information("Intentanto obtener todas las reviews");
             return Ok(await _reviewService.ObtenerTodasLasReviews());
         }
-        
 
+        [HttpGet("obtener-review/{id}")]
+        public async Task<IActionResult> ObtenerReviewPorId(int id) 
+        {
+            Log.Information("Intentando obtener un review por su id: {0}",id);
+
+            var review = await _reviewService.ObtenerReviewPorId(id);
+
+            Log.Information("Review obtenida con exito: {0}",review.Titulo);
+            return Ok(review);
+        }
 
 
     }
